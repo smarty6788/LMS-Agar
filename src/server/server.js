6b919e8cfba7840766725973c2276c963557dac3
finwaals <15fjmills@uctc.org.uk>
2016-01-27 08:28:15 +0000

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -334,8 +334,8 @@ io.on('connection', function (socket) {
     socket.on('pass', function(data) {
         if (data[0] === c.adminPass) {
             console.log('[ADMIN] ' + currentPlayer.name + ' just logged in as an admin!');
-            socket.emit('serverMSG', 'Welcome back ' + currentPlayer.name);
-            socket.broadcast.emit('serverMSG', currentPlayer.name + ' just logged in as admin!');
+            socket.emit('serverMSG', 'You have logged in as admin, ' + currentPlayer.name);
+            //socket.broadcast.emit('serverMSG', currentPlayer.name + ' just logged in as admin!');
             currentPlayer.admin = true;
         } else {
             console.log('[ADMIN] ' + currentPlayer.name + ' attempted to log in with incorrect password.');
